198aad67c89c3ef9d8227fdb761d156ba656b93d
Baebeemoe <122005586+baebeemoe@users.noreply.github.com>
2023-03-24 20:06:47 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        EmployeeDetails Employee = new EmployeeDetails();
+        AttendanceRecord Attendance = new AttendanceRecord();
+    }
+
+    public double calcWeeklySalary;
+    public double calcGrossSalary;
+    public double calcNetSalary;
+
+}
