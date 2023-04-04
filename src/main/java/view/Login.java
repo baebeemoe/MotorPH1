7e203c0e314f6488a68dae5f77485528d1f12f60
henson <hensonlao@gmail.com>
2023-04-04 13:59:13 +0800

    Created starting files for login form

---
+++ src/main/java/view/Login.java
@@ -0,0 +1,36 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+    private JPanel LoginForm;
+    private JPanel backgroundPanel;
+    private JPanel formContainer;
+    private JLabel loginTitle;
+    private JPanel form;
+    private JTextField emailField;
+    private JPasswordField passwordField;
+    private JLabel emailLabel;
+    private JLabel passwordLabel;
+
+    public Login() {
+        super("MotorPH - Payroll Management System");
+        setContentPane(LoginForm);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(700, 500);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+}
